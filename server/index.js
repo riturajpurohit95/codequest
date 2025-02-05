903b2c9f4e4729eb1933c418396d6ceccf5eccb2
riturajpurohit95 <riturajpurohit95@gmail.com>
2025-02-05 07:47:36 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import dotenv from "dotenv"
+import userroutes from "./routes/user.js"
+import questionroutes from "./routes/question.js"
+import answerroutes from "./routes/answer.js"
+
+const app = express();
+dotenv.config();
+app.use(express.json({ limit: "30mb", extended: true }))
+app.use(express.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors());
+
+
+
+
+app.use("/user", userroutes);
+app.use('/questions', questionroutes)
+app.use('/answer',answerroutes)
+app.get('/', (req, res) => {
+    res.send("Codequest is running perfect")
+})
+
+const PORT = process.env.PORT || 5000
+const database_url = process.env.MONGODB_URL
+// const database_url = "mongodb+srv://admin:test@cluster0.n11yc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+// mongoose.connect(database_url)
+//     .then(() => app.listen(PORT, () => { console.log(`server running on port ${PORT}`) }))
+//     .catch((err) => console.log(err.message))
+
+mongoose.connect(database_url)
+    .then(() => {
+        console.log("✅ Connected to MongoDB Successfully!");
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    })
+    .catch((err) => {
+        console.error("❌ MongoDB Connection Error:", err.message);
+    });
